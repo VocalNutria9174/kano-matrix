5880be66db318a7309192f4efbffd39af187a5cb
VocalNutria9174 <cudaryb@gmail.com>
2025-05-16 14:55:19 -0400

    Update judoka.js

--- src/judoka.js
+++ src/judoka.js
@@ -1,21 +1,5 @@
 var Judoka = function (canvas, context, fontSize) {
-    this.ascii = [
-        "              a8888b.        ",
-        "             d888888b.       ",
-        "             8P"YP"Y88       ",
-        "             8|o||o|88       ",
-        "             8'    .88       ",
-        "             8`._.' Y8.      ",
-        "            d/      `8b.     ",
-        "          .dP   .     Y8b.   ",
-        "         d8:'   "   `::88b.  ",
-        "        d8"           `Y88b  ",
-        "       :8P     '       :888  ",
-        "        8a.    :      _a88P  ",
-        "      ._/"Yaa_ :    .| 88P|  ",
-        "      \    YP"      `| 8P  `.",
-        "      /     \._____.d|    .' ",
-        "      `--..__)888888P`._.'   " ]
+    this.ascii = ["              a8888b.        ","             d888888b.       ","             8P"YP"Y88       ","             8|o||o|88       ","             8'    .88       ","             8`._.' Y8.      ","            d/      `8b.     ","          .dP   .     Y8b.   ","         d8:'   "   `::88b.  ","        d8"           `Y88b  ","       :8P     '       :888  ","        8a.    :      _a88P  ","      ._/"Yaa_ :    .| 88P|  ","      \    YP"      `| 8P  `.","      /     \._____.d|    .' ","      `--..__)888888P`._.'   " ];
 
     /* The mask contains a flag for each line that determines whether
        it should be displayed or not. These flags are randomly changed
